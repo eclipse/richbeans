4557e3f655e108497fe4c39a17ab6d83f9ed29d6
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2014-09-19 18:20:16 +0100

    Add new IImageStitchingProcess interface

---
+++ org.eclipse.dawnsci.analysis.api/src/org/eclipse/dawnsci/analysis/api/image/IImageStitchingProcess.java
@@ -0,0 +1,40 @@
+/*-
+ * Copyright (c) 2014 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.dawnsci.analysis.api.image;
+
+import java.util.List;
+
+import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
+
+/**
+ * This service can be called to process IDataset using BoofCV stitching algorithms
+ * 
+ * @author wqk87977
+ *
+ */
+public interface IImageStitchingProcess {
+
+	/**
+	 * Stitches a list of images with a default matrix size
+	 * 
+	 * @param input
+	 * @return output stitched image
+	 */
+	public IDataset stitch(List<IDataset> input);
+
+	/**
+	 * Stitches a list of images
+	 * 
+	 * @param input
+	 * @param matrixSize
+	 * @return output stitched image
+	 */
+	public IDataset stitch(List<IDataset> input, int matrixSize);
+
+}
