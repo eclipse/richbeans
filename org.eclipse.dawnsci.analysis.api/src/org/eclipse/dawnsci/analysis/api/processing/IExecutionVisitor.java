9261ffd713b2b0a9b945d79f44030fb7d5d543fe
matthew.gerring <matthew.gerring@gmail.com>
2014-10-31 16:29:14 +0000

    Added event for visitor.

--- org.eclipse.dawnsci.analysis.api/src/org/eclipse/dawnsci/analysis/api/processing/IExecutionVisitor.java
+++ org.eclipse.dawnsci.analysis.api/src/org/eclipse/dawnsci/analysis/api/processing/IExecutionVisitor.java
@@ -9,10 +9,6 @@
 
 package org.eclipse.dawnsci.analysis.api.processing;
 
-import org.eclipse.dawnsci.analysis.api.dataset.Slice;
-import org.eclipse.dawnsci.analysis.api.metadata.OriginMetadata;
-import org.eclipse.dawnsci.analysis.api.monitor.IMonitor;
-import org.eclipse.dawnsci.analysis.api.processing.model.IOperationModel;
 
 /**
  * This interface is designed to be called when a series of operations has been 
@@ -26,10 +22,23 @@ public interface IExecutionVisitor {
 	/**
 	 * Initialise the execution visitor with the series of operations that are going to run
 	 *  
-	 * @param series
 	 * @throws Exception
 	 */
-	public void init(IOperation<? extends IOperationModel, ? extends OperationData>[] series, OriginMetadata origin) throws Exception;
+	public void init(ExecutionEvent evt) throws Exception;
+	
+    /**
+     * Called when an execution in the pipeline has run, before the end	but after a given operation.
+     * 
+     * Provides the option of saving the steps information to a file if required.
+     * 
+     */
+	public void notify(ExecutionEvent evt);
+	
+	/**
+	 * Called when the series of operations has been done, with the current slice
+	 */
+	public void executed(ExecutionEvent evt) throws Exception;
+	
 	
 	/**
 	 * Tell the execution visitor to close, releasing its resources.
@@ -39,40 +48,22 @@ public interface IExecutionVisitor {
 	 * @throws Exception
 	 */
 	public void close() throws Exception;
-	
-    /**
-     * Called when an execution in the pipeline has run, before the end	but after a given operation.
-     * 
-     * Provides the option of saving the steps information to a file if required.
-     * 
-     * @param intermediateData
-     * @param data
-     * @param dataDims TODO
-     */
-	public void notify(IOperation<? extends IOperationModel, ? extends OperationData> intermediateData, OperationData data, Slice[] slices, int[] shape, int[] dataDims);
-	
-	/**
-	 * Called when the series of operations has been done, with the current slice
-	 * @param result
-	 * @param dataDims TODO
-	 */
-	public void executed(OperationData result, IMonitor monitor, Slice[] slices, int[] shape, int[] dataDims) throws Exception;
-	
+
 	
 	public class Stub implements IExecutionVisitor {
 
 		@Override
-		public void executed(OperationData result, IMonitor monitor, Slice[] slices, int[] shape, int[] dataDims) throws Exception {
+		public void executed(ExecutionEvent evt) throws Exception {
 			
 		}
 
 		@Override
-		public void notify(IOperation<? extends IOperationModel, ? extends OperationData> intermeadiateData, OperationData data, Slice[] slices, int[] shape, int[] dataDims) {
+		public void notify(ExecutionEvent evt) {
 			
 		}
 
 		@Override
-		public void init(IOperation<? extends IOperationModel, ? extends OperationData>[] series, OriginMetadata origin) {
+		public void init(ExecutionEvent evt) {
 			
 		}
 
