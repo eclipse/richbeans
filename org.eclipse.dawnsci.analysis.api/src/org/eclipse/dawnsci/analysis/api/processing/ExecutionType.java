7390e847c853a2de19c7eb5830bb88189d39a58c
matthew.gerring <matthew.gerring@gmail.com>
2014-11-20 11:35:09 +0000

    Added enum

---
+++ org.eclipse.dawnsci.analysis.api/src/org/eclipse/dawnsci/analysis/api/processing/ExecutionType.java
@@ -0,0 +1,8 @@
+package org.eclipse.dawnsci.analysis.api.processing;
+
+/**
+ * Types of execution when IOperationService.execute is called.
+ */
+public enum ExecutionType {
+	SERIES, PARALLEL, GRAPH;
+}
