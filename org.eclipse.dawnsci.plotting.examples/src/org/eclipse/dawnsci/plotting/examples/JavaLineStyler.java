cf4f1ac0f82b84c64c69d5424b19dc486790ee85
matthew.gerring <matthew.gerring@gmail.com>
2014-11-21 09:46:43 +0000

    Updated header

--- org.eclipse.dawnsci.plotting.examples/src/org/eclipse/dawnsci/plotting/examples/JavaLineStyler.java
+++ org.eclipse.dawnsci.plotting.examples/src/org/eclipse/dawnsci/plotting/examples/JavaLineStyler.java
@@ -1,11 +1,14 @@
-/*******************************************************************************
- * Copyright (c) 2000, 2003 IBM Corporation and others. All rights reserved.
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors: IBM Corporation - initial API and implementation
- ******************************************************************************/
+/*-
+ *******************************************************************************
+ * Copyright (c) 2011, 2014 Diamond Light Source Ltd.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Matthew Gerring - initial API and implementation and/or initial documentation
+ *******************************************************************************/
 
 /**
  * @see http://www.java2s.com/Code/Java/SWT-JFace-Eclipse/JavaSourcecodeViewer.htm
