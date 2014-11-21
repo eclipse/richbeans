003e6a2774bda4d2405ef86e7fa398b7034740fa
matthew.gerring <matthew.gerring@gmail.com>
2014-11-21 10:24:20 +0000

    Fixed header

--- org.eclipse.dawnsci.doe/src/org/eclipse/dawnsci/doe/package-info.java
+++ org.eclipse.dawnsci.doe/src/org/eclipse/dawnsci/doe/package-info.java
@@ -1,12 +1,15 @@
-/*
- * Copyright (c) 2012 European Synchrotron Radiation Facility,
- *                    Diamond Light Source Ltd.
- *
+/*-
+ *******************************************************************************
+ * Copyright (c) 2011, 2014 Diamond Light Source Ltd.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- */ 
+ *
+ * Contributors:
+ *    Matthew Gerring - initial API and implementation and/or initial documentation
+ *******************************************************************************/ 
+
 /**
 
 The doe package in concerned with Design of Experiment logic used to create ranges of experiments.
