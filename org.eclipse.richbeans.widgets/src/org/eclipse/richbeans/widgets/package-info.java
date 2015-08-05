78dc68317f2607b5abfb34f6b07edaa84de2400f
Matthew Gerring <matthew.gerring@diamond.ac.uk>
2015-08-05 15:44:41 +0100

    Added license files and file headers to all .java files.

--- org.eclipse.richbeans.widgets/src/org/eclipse/richbeans/widgets/package-info.java
+++ org.eclipse.richbeans.widgets/src/org/eclipse/richbeans/widgets/package-info.java
@@ -1,6 +1,16 @@
-/**-
- * Copyright © 2010 Diamond Light Source Ltd.
+/*-
+ *******************************************************************************
+ * Copyright (c) 2011, 2014 Diamond Light Source Ltd.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
+ * Contributors:
+ *    Matthew Gerring - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+/**-
+
                                   RICH BEAN FRAMEWORK - v1.0.5
 
 Introduction
