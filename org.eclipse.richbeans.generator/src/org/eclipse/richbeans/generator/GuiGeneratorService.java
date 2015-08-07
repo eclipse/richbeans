a25b4fe5108f05d6aac71dd2b3aa974ef302d3c7
Colin Palmer <colin.palmer@diamond.ac.uk>
2015-08-07 14:15:50 +0100

    Very basic initial implementation of generateGui()

--- org.eclipse.richbeans.generator/src/org/eclipse/richbeans/generator/GuiGeneratorService.java
+++ org.eclipse.richbeans.generator/src/org/eclipse/richbeans/generator/GuiGeneratorService.java
@@ -1,14 +1,28 @@
 package org.eclipse.richbeans.generator;
 
 import org.eclipse.richbeans.api.generator.IGuiGeneratorService;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.metawidget.inspectionresultprocessor.commons.jexl.JexlInspectionResultProcessor;
+import org.metawidget.inspectionresultprocessor.iface.InspectionResultProcessor;
+import org.metawidget.swt.SwtMetawidget;
+import org.metawidget.swt.widgetprocessor.binding.databinding.DataBindingProcessor;
 
 public class GuiGeneratorService implements IGuiGeneratorService {
 
 	@Override
 	public Composite generateGui(Object bean, Composite parent) {
-		// TODO Auto-generated method stub
-		return null;
+
+		SwtMetawidget metawidget = new SwtMetawidget(parent, SWT.NONE);
+
+		// TODO data binding!
+
+		InspectionResultProcessor<SwtMetawidget> jexlProcessor = new JexlInspectionResultProcessor<SwtMetawidget>();
+		metawidget.addInspectionResultProcessor(jexlProcessor);
+
+		metawidget.setToInspect(bean);
+
+		return metawidget;
 	}
 
 }
