23cfd653a36f26ac487f7783d31b4f593a5edd6d
Colin Palmer <colin.palmer@diamond.ac.uk>
2015-08-07 13:57:59 +0100

    Initial commit of GUI Generator service

---
+++ org.eclipse.richbeans.generator/src/org/eclipse/richbeans/generator/GuiGeneratorService.java
@@ -0,0 +1,14 @@
+package org.eclipse.richbeans.generator;
+
+import org.eclipse.richbeans.api.generator.IGuiGeneratorService;
+import org.eclipse.swt.widgets.Composite;
+
+public class GuiGeneratorService implements IGuiGeneratorService {
+
+	@Override
+	public Composite generateGui(Object bean, Composite parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
