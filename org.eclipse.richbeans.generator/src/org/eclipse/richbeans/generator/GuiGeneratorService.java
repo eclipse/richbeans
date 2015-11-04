51789db803a7e826831f8a8da4b4188ce067efb9
James Mudd <james.mudd@diamond.ac.uk>
2015-11-04 16:30:36 +0000

    Add a widget builder to create controls for scannables for live stage controls
    This adds a dependency on GDA plugins potentially not good for keeping the
    GUI generator as generic as possible and plain SWT. Think we shoudl refactor
    this out eventually.

--- org.eclipse.richbeans.generator/src/org/eclipse/richbeans/generator/GuiGeneratorService.java
+++ org.eclipse.richbeans.generator/src/org/eclipse/richbeans/generator/GuiGeneratorService.java
@@ -3,6 +3,7 @@ package org.eclipse.richbeans.generator;
 import org.eclipse.richbeans.api.generator.IGuiGeneratorService;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.metawidget.inspectionresultprocessor.commons.jexl.JexlInspectionResultProcessor;
 import org.metawidget.inspectionresultprocessor.iface.InspectionResultProcessor;
 import org.metawidget.inspector.annotation.MetawidgetAnnotationInspector;
@@ -10,9 +11,14 @@ import org.metawidget.inspector.composite.CompositeInspector;
 import org.metawidget.inspector.composite.CompositeInspectorConfig;
 import org.metawidget.inspector.propertytype.PropertyTypeInspector;
 import org.metawidget.swt.SwtMetawidget;
+import org.metawidget.swt.widgetbuilder.ReadOnlyWidgetBuilder;
+import org.metawidget.swt.widgetbuilder.SwtWidgetBuilder;
+import org.metawidget.widgetbuilder.composite.CompositeWidgetBuilder;
+import org.metawidget.widgetbuilder.composite.CompositeWidgetBuilderConfig;
 
 public class GuiGeneratorService implements IGuiGeneratorService {
 
+	@SuppressWarnings("unchecked") // setWidgetBuilders takes varargs and should be annotated see line 44 CompositeWidgetBuilderConfig
 	@Override
 	public Composite generateGui(Object bean, Composite parent) {
 
@@ -32,7 +38,17 @@ public class GuiGeneratorService implements IGuiGeneratorService {
 		metawidget.addInspectionResultProcessor(jexlProcessor);
 
 		// 3. WidgetBuilder
-		// (default)
+		// Create a composite widget builder they will be called in turn until one returns a Control
+		CompositeWidgetBuilderConfig<Control, SwtMetawidget> compositeWidgetBuilderConfig = new CompositeWidgetBuilderConfig<Control, SwtMetawidget>();
+		// Compiler warnings are suppressed here due to varargs, see note above
+		compositeWidgetBuilderConfig.setWidgetBuilders(
+				// GDA Custom builders
+				new ScannableWidgetBuilder(),
+				// Metawidget standard builders
+				new ReadOnlyWidgetBuilder(),
+				new SwtWidgetBuilder());
+		// Set the composite widget builder to be used
+		metawidget.setWidgetBuilder(new CompositeWidgetBuilder<Control, SwtMetawidget>(compositeWidgetBuilderConfig));
 
 		// 4. WidgetProcessors
 
@@ -52,6 +68,7 @@ public class GuiGeneratorService implements IGuiGeneratorService {
 
 		// 5. Layout
 		// (default)
+		// e.g metawidget.setMetawidgetLayout(layout);
 
 		// Finish
 		metawidget.setToInspect(bean);
