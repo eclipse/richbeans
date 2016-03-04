9a8d32cf6ebdbf1572112a166c82757544101dac
Colin Palmer <colin.palmer@diamond.ac.uk>
2016-03-04 16:05:26 +0000

    Add Javadoc to SWTTestBase
    https://github.com/eclipse/richbeans/issues/24

--- org.eclipse.richbeans.generator.test/src/org/eclipse/richbeans/generator/SWTTestBase.java
+++ org.eclipse.richbeans.generator.test/src/org/eclipse/richbeans/generator/SWTTestBase.java
@@ -9,6 +9,15 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 
+/**
+ * Helper class for running SWT unit tests.
+ * <p>
+ * For use in the standard ant build (where a physical display is not usually available), the ant script and testing
+ * classpath need to be set up correctly to initialize a display and ensure the correct version of SWT is available.
+ * The releng.ant file in this bundle provides an example of how to do this.
+ *
+ * @author Colin Palmer
+ */
 public class SWTTestBase {
 
 	protected static Display display;
